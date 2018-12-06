5b28f388eaa5cd8ad30dc462d08892ae4e91915b
Justen Quirante <jcquirante@gmail.com>
2018-12-05 16:17:11 -0800

    Added ajax call to tmbd

--- main.js
+++ main.js
@@ -2,4 +2,20 @@ $(document).ready(initializeApp);
 
 function initializeApp(){
     console.log('Initializing App...');
+    getDataFromServer();
+}
+
+function getDataFromServer() {
+    console.log('hi');
+    var settings = {
+        url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=fb2158f8324ad535f0c817ef2fb98040',
+        dataType: 'json',
+        method: 'get',
+        success: function(response) {
+            console.log(response);
+        }
+
+    }
+
+    $.ajax(settings);
 }
