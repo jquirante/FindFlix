3080cc72a5f66f4f93a74a100e653fd35d18485d
Kur0a5h <kuroash.esmaili@gmail.com>
2018-12-06 18:56:36 -0800

    linked htmls

--- main.js
+++ main.js
@@ -8,7 +8,7 @@ function initializeApp() {
     
 
     var modal = $('.modalPageContainer');
-
+    
     $(".close-modal").click((event)=>{
         console.log(event);
         if($(event.target).hasClass("close-modal")) {
--- moviePoster.js
+++ moviePoster.js
@@ -87,8 +87,10 @@ class MoviePosterService {
             getActorInformation(movieId);
             getVideos(title);
             $(".modalPageContainer").css('display', 'block');
+            
         }
     }
+    
 }
 var ajaxOptionsGetPoster = {
     url: 'https://api.themoviedb.org/3/movie/now_playing?api_key=fb2158f8324ad535f0c817ef2fb98040',
